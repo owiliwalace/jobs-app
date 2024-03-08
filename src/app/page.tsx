cc306690736c99526e1ece9d9d5b9b9b2070d2e9
owiliwalace <owiliwalace@gmail.com>
2024-03-08 10:36:20 +0300

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,7 +60,7 @@ const NavigationMenuDemo =() =>{
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+          <NavigationMenuTrigger>Veronica Jobs-App</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
               <li className="row-span-3">
@@ -71,29 +71,20 @@ const NavigationMenuDemo =() =>{
                   >
                  
                     <div className="mb-2 mt-4 text-lg font-medium">
-                      shadcn/ui
+                      Employee
                     </div>
                     <p className="text-sm leading-tight text-muted-foreground">
-                      Beautifully designed components built with Radix UI and
-                      Tailwind CSS.
+                      Employee Management website for temporary workers.
                     </p>
                   </a>
                 </NavigationMenuLink>
               </li>
-              <ListItem href="/docs" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href="/docs/installation" title="Installation">
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem href="/docs/primitives/typography" title="Typography">
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
+              
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+          <NavigationMenuTrigger>About Us</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
               {components.map((component) => (
@@ -111,13 +102,11 @@ const NavigationMenuDemo =() =>{
         <NavigationMenuItem>
           <Link href="/about" legacyBehavior passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              About Us
+              GET STARTED
             </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
-<div>
-LOGIN
-</div>
+
       </NavigationMenuList>
     </NavigationMenu>
 <Hero />
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,8 +1,24 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <>
+    <div className=" h-[500px] w-full bg-white flex">
+      <div className="h-[500px] w-1/2">
+        WE
+      </div>
+    <Image
+    className=' w-1/2'
+      src="/hero-homepage.jpg"
+      width={400}
+      height={400}
+      alt="image one"
+    />
+   
+     
+    </div>
+    </>
   )
 }
 
