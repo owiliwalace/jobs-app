d6c427618397edf3d1f32eb9d476c92bdcb826a6
unknown <owiliwalace@gmail.com>
2024-03-11 18:21:45 +0300

    6:20PM

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,46 +1,9 @@
-"use client"
-import {db}from "../firebaseConfig"
-import { collection, getDocs } from "firebase/firestore"
+import React from 'react'
 
-import React,{useState, useEffect} from 'react';
-
-async function fetchDataFromFirestore() {
-  const querySnapshot = await getDocs(collection(db,"messages"))
-  
-  const data=[];
-  querySnapshot.forEach((doc) => {
-    data.push({id:doc.id, ...doc.data()});
-
-  });
-  return data;
-  
-}
-
-const About = () => {
-  const [userData, setUserData]=useState([]);
-  useEffect(() => {
-    async function fetchData() {
-      const data =await fetchDataFromFirestore();
-      setUserData(data);
-      
-    }
-    fetchData();
-  },[]);
+const page = () => {
   return (
-    <>
-    
-    <div className="">
-      {userData.map((user) => (
-        <div key={user.id} className="mb-4">
-          <p className="text-xl font-semibold">{user.name}</p>
-          <p className="text-xl font-semibold">{user.email}</p>
-          <p className="text-xl font-semibold">{user.message}</p>
-        </div>
-      ))}
-    </div>
-
-    </>
+    <div>page</div>
   )
 }
 
-export default About
+export default page
